100db835858150c29d61956f9d1bcca8878cf1ce
Ashish <ashishchalise@gmail.com>
2018-01-24 13:56:15 -0600

    Maven Preject to use in Git and Jenkins

---
+++ maven-project-master/server/src/main/java/com/example/Greeter.java
@@ -0,0 +1,29 @@
+package com.example;
+
+/**
+ * This is a class.
+ */
+public class Greeter {
+
+  /**
+   * This is a constructor.
+   */
+  public Greeter() {
+
+  }
+
+  //TODO: Add javadoc comment
+  public String greet(String someone) {
+    return String.format("Hello world something else, %s!", someone);
+  }
+  
+  public String greet2(String someone) {
+	    return String.format("Hello USA , %s!", someone);
+	  }
+  
+  public String greet3(String someone) {
+	    return String.format("Hello India , %s!", someone);
+	  }
+  
+
+}
